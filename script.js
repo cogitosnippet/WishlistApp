24d77e5b2f5354aba41f8f7d67a97a1ac6dedc93
Gautam <gauta.m.endiratta@gmial.com>
2024-05-14 00:40:09 +0530

    Updated

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function uniqueId() {
+  let number = Math.ceil(Math.random() * 10000);
+  return number.toString();
+}
+
 let addButton = document.getElementById('button');
 let listInput = document.getElementById('input');
 let listContainer = document.getElementById('list-container');
@@ -6,11 +11,6 @@ let listContainer = document.getElementById('list-container');
 let todo;
 let localData = JSON.parse(localStorage.getItem("todo"));
 let todoList = localData || [];
-
-function uniqueId() {
-  let number = Math.ceil(Math.random() * 10000);
-  return number.toString();
-}
 
 addButton.addEventListener('click',(e)=>{
   e.preventDefault();
@@ -23,7 +23,27 @@ addButton.addEventListener('click',(e)=>{
     listInput.value ="";
 })
 
+listContainer.addEventListener('click',(e)=>{
+  let key = e.target.dataset.key;
+  let toDelete = e.target.dataset.todokey ;
+  todoList.map(todo => {
+    if(key===todo.id){
+      todo.isCompleted = !todo.isCompleted
+    }
+  })
+  todoList = todoList.filter(todo => todo.id != toDelete)
+  renderListContainer(todoList);
+  localStorage.setItem("todo", JSON.stringify(todoList));
 
+})
+
+
+function renderListContainer(todoList){
+  let item = todoList.map(({id,todo,isCompleted})=>{
+    return `<div class ="margin"><div class="flex"><input type ="checkbox" id="item-${id}" class="wishCheck" data-key = ${id} ${isCompleted ? "checked": ""}><label for="item-${id}" class="wishName ${isCompleted ? "checked-todo": ""}" data-key=${id}>${todo}</label></div><img src="icons8-delete-50.png" data-todokey = ${id} class ="cursor"></div>`
+  })
+  listContainer.innerHTML = item.join("");
+}
 function renderListContainer(todoList){
   let item = todoList.map(({id,todo,isCompleted})=>{
     return `<div class ="margin"><div class="flex"><input type ="checkbox" id="item-${id}" class="wishCheck" data-key = ${id} ${isCompleted ? "checked": ""}><label for="item-${id}" class="wishName ${isCompleted ? "checked-todo": ""}" data-key=${id}>${todo}</label></div><img src="icons8-delete-50.png" data-todokey = ${id} class ="cursor"></div>`
